311e57f18b2b4c54c6b8ff5e603e0846b2bdd4f6
kiyotaka-moriya <kiyotaka.moriya@gmail.com>
2025-10-29 18:28:09 +0900

    各バリデーション修正

--- src/app/Http/Controllers/RegisteredUserController.php
+++ src/app/Http/Controllers/RegisteredUserController.php
@@ -17,14 +17,13 @@ class RegisteredUserController extends Controller
       'password' => 'required|string|min:8|confirmed',
     ], [
       // ユーザー名のバリデーションメッセージ
-      'name.required' => 'ユーザー名を入力してください',
-      'name.string' => 'ユーザー名は文字列で入力してください',
-      'name.max' => 'ユーザー名は255文字以内で入力してください',
+      'name.required' => 'お名前を入力してください',
+      'name.string' => 'お名前は文字列で入力してください',
+      'name.max' => 'お名前は255文字以内で入力してください',
 
       // メールアドレスのバリデーションメッセージ
       'email.required' => 'メールアドレスを入力してください',
-      'email.string' => 'メールアドレスは文字列で入力してください',
-      'email.email' => '有効なメールアドレス形式で入力してください',
+      'email.email' => 'メールアドレスはメール形式で入力してください',
       'email.max' => 'メールアドレスは255文字以内で入力してください',
       'email.unique' => 'このメールアドレスは既に登録されています',
 
@@ -32,7 +31,7 @@ class RegisteredUserController extends Controller
       'password.required' => 'パスワードを入力してください',
       'password.string' => 'パスワードは文字列で入力してください',
       'password.min' => 'パスワードは8文字以上で入力してください',
-      'password.confirmed' => 'パスワードと確認用パスワードが一致しません',
+      'password.confirmed' => 'パスワードと一致しません',
     ]);
 
     $user = User::create([
